134e2a448d7a45629bd4fbda36e567d3b165268e
r351574nc3 <r351574nc3@gmail.com>
2015-04-01 01:27:21 -0700

    Adding initial maven project

---
+++ src/main/java/com/github/r351574nc3/maven/App.java
@@ -0,0 +1,13 @@
+package com.github.r351574nc3.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
